6e122bfd9423d2400cbb8c8898719ce3add0773c
naimhakimov13 <108753643+naimhakimov13@users.noreply.github.com>
2024-07-03 18:24:05 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,9 @@ URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="
 *New MR*: 12313123123
 *From*:+123123123
+"
+
+echo $TELEGRAM_BOT_TOKEN
+echo $TELEGRAM_CHAT_ID
 
 /bin/curl -s --max-time $TIME -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$TEXT" -d "parse_mode=markdown" -d "disable_web_page_preview=1" "$URL" > /dev/null
